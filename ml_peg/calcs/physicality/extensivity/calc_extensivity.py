e394eaa6cd08a491a3e5cd8de20a652498c4b241
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-10-31 18:13:26 +0000

    Use float64 for extensivity test

--- ml_peg/calcs/physicality/extensivity/calc_extensivity.py
+++ ml_peg/calcs/physicality/extensivity/calc_extensivity.py
@@ -60,6 +60,8 @@ def test_extensivity(mlip: tuple[str, Any]) -> None:
         Name of model use and model to get calculator.
     """
     model_name, model = mlip
+    # Use double precision
+    model.default_dtype = "float64"
     calc = model.get_calculator()
 
     sym1, sym2 = "Al", "Ni"  # element of slab-1 and slab-2
