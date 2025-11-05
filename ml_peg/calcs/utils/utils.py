1c714cb6d32e616a2ed475809fb8352ca398543e
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-11-05 20:02:00 +0000

    Split download from S3 and GitHub

--- ml_peg/calcs/utils/utils.py
+++ ml_peg/calcs/utils/utils.py
@@ -10,59 +10,104 @@ import zipfile
 
 import requests
 
-# GitHub API URL (for listing contents if needed)
-BENCHMARK_DATA_URL = (
-    "https://api.github.com/repos/joehart2001/mlipx/contents/benchmark_data"
-)
-
-# Raw download URL (for direct downloading)
-BENCHMARK_DATA_DOWNLOAD_URL = (
-    "https://raw.githubusercontent.com/joehart2001/mlipx/main/benchmark_data/"
-)
+from ml_peg.data.data import download
 
 # Local cache directory
-BENCHMARK_DATA_DIR = pathlib.Path.home() / ".cache" / "my_benchmark"
+BENCHMARK_DATA_DIR = pathlib.Path.home() / ".cache" / "ml_peg"
 
 
-def get_benchmark_data(name: str, force: bool = False) -> Path:
+def download_s3_data(
+    key: str,
+    filename: str | Path,
+    bucket: str = "ml-peg-data",
+    endpoint: str = "https://s3.echo.stfc.ac.uk",
+    force: bool = False,
+) -> None:
     """
-    Retrieve benchmark data.
+    Download data from an S3 bucket.
+
+    Parameters
+    ----------
+    key
+        Name of file in S3 bucket to download.
+    filename
+        Name of file to save download as locally.
+    bucket
+        Name of S3 bucket. Default is "ml-peg-data".
+    endpoint
+        Endpoint URL. Default is "https://s3.echo.stfc.ac.uk".
+    force
+        Whether to ignored cached download. Default is False.
+    """
+    local_path = Path(BENCHMARK_DATA_DIR) / filename
+
+    # Download file if not already cached or if force is True
+    if force or not local_path.exists():
+        print(f"[download] Downloading {endpoint}/{bucket}/{filename}")
+        # Write download file and extract if necessary
+        download(key=key, filename=local_path, bucket=bucket, endpoint=endpoint)
+        extract_zip(local_path)
+    else:
+        print(f"[cache] Found cached file: {local_path.name}")
+
+
+def download_github_data(filename: str, github_uri: str, force: bool = False) -> Path:
+    """
+    Retrieve benchmark data from a GitHub repository.
 
     If it's a .zip, download and extract it.
 
     Parameters
     ----------
-    name
+    filename
         Name of benchmark data file.
+    github_uri
+        Name of GitHub URI to download data from.
     force
-        Whether to ignore cached download.
+        Whether to ignore cached download. Default is False.
 
     Returns
     -------
     Path
         Path to extracted data.
     """
-    uri = f"{BENCHMARK_DATA_DOWNLOAD_URL}/{name}"
-    local_path = Path(BENCHMARK_DATA_DIR) / name
+    uri = f"{github_uri}/{filename}"
+    local_path = Path(BENCHMARK_DATA_DIR) / filename
 
     # Download file if not already cached or if force is True
     if force or not local_path.exists():
-        print(f"[download] Downloading {name} from {uri}")
+        print(f"[download] Downloading {filename} from {uri}")
+
         response = requests.get(uri)
         response.raise_for_status()
         local_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # Write contents and extract if necessary
         with open(local_path, "wb") as f_out:
             f_out.write(response.content)
+        extract_zip(local_path)
+
     else:
         print(f"[cache] Found cached file: {local_path.name}")
 
-    # If it's a zip, extract it
-    if local_path.suffix == ".zip":
-        extract_dir = local_path.parent
-        with zipfile.ZipFile(local_path, "r") as zip_ref:
-            zip_ref.extractall(extract_dir)
-        return extract_dir
-    raise ValueError(f"Unsupported file format: {local_path}")
+
+def extract_zip(filename: Path) -> None:
+    """
+    Attempt to extract a zip file.
+
+    Parameters
+    ----------
+    filename
+        Name of potential zip file to extract.
+    """
+    # If it's a zip, try to extract it
+    if filename.suffix == ".zip":
+        extract_dir = filename.parent
+        try:
+            with zipfile.ZipFile(filename, "r") as zip_ref:
+                zip_ref.extractall(extract_dir)
+        except (ValueError, RuntimeError, zipfile.BadZipFile) as err:
+            raise ValueError(f"Unable to unzip file: {filename}") from err
 
 
 @contextlib.contextmanager
