12d416ee294d51c0853e9129ceeb1c92f60fe184
Elliott Kasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-11-06 14:01:58 +0000

    Add X23 benchmark (#118)

---
+++ ml_peg/app/molecular_crystal/X23/app_X23.py
@@ -0,0 +1,88 @@
+"""Run X23 app."""
+
+from __future__ import annotations
+
+from dash import Dash
+from dash.html import Div
+
+from ml_peg.app import APP_ROOT
+from ml_peg.app.base_app import BaseApp
+from ml_peg.app.utils.build_callbacks import (
+    plot_from_table_column,
+    struct_from_scatter,
+)
+from ml_peg.app.utils.load import read_plot
+from ml_peg.models.get_models import get_model_names
+from ml_peg.models.models import current_models
+
+MODELS = get_model_names(current_models)
+BENCHMARK_NAME = "X23 Lattice Energies"
+DOCS_URL = (
+    "https://ddmms.github.io/ml-peg/user_guide/benchmarks/molecular_crystal.html#x23"
+)
+DATA_PATH = APP_ROOT / "data" / "molecular_crystal" / "X23"
+
+
+class X23App(BaseApp):
+    """X23 benchmark app layout and callbacks."""
+
+    def register_callbacks(self) -> None:
+        """Register callbacks to app."""
+        scatter = read_plot(
+            DATA_PATH / "figure_lattice_energies.json",
+            id=f"{BENCHMARK_NAME}-figure",
+        )
+
+        # Assets dir will be parent directory - individual files for each system
+        structs_dir = DATA_PATH / MODELS[0]
+        structs = [
+            f"assets/molecular_crystal/X23/{MODELS[0]}/{struct_file.stem}.xyz"
+            for struct_file in sorted(structs_dir.glob("*.xyz"))
+        ]
+
+        plot_from_table_column(
+            table_id=self.table_id,
+            plot_id=f"{BENCHMARK_NAME}-figure-placeholder",
+            column_to_plot={"MAE": scatter},
+        )
+
+        struct_from_scatter(
+            scatter_id=f"{BENCHMARK_NAME}-figure",
+            struct_id=f"{BENCHMARK_NAME}-struct-placeholder",
+            structs=structs,
+            mode="struct",
+        )
+
+
+def get_app() -> X23App:
+    """
+    Get X23 benchmark app layout and callback registration.
+
+    Returns
+    -------
+    X23App
+        Benchmark layout and callback registration.
+    """
+    return X23App(
+        name=BENCHMARK_NAME,
+        description="Lattice energies for 23 organic molecular crystals.",
+        docs_url=DOCS_URL,
+        table_path=DATA_PATH / "x23_metrics_table.json",
+        extra_components=[
+            Div(id=f"{BENCHMARK_NAME}-figure-placeholder"),
+            Div(id=f"{BENCHMARK_NAME}-struct-placeholder"),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    # Create Dash app
+    full_app = Dash(__name__, assets_folder=DATA_PATH.parent.parent)
+
+    # Construct layout and register callbacks
+    x23_app = get_app()
+    full_app.layout = x23_app.layout
+    x23_app.register_callbacks()
+
+    # Run app
+    full_app.run(port=8053, debug=True)
---
+++ ml_peg/calcs/molecular_crystal/X23/calc_X23.py
@@ -0,0 +1,76 @@
+"""Run calculations for X23 tests."""
+
+from __future__ import annotations
+
+from copy import copy
+from pathlib import Path
+from typing import Any
+
+from ase import units
+from ase.io import read, write
+import numpy as np
+import pytest
+
+from ml_peg.calcs.utils.utils import get_benchmark_data
+from ml_peg.models.get_models import load_models
+from ml_peg.models.models import current_models
+
+MODELS = load_models(current_models)
+
+DATA_PATH = Path(__file__).parent / "data"
+OUT_PATH = Path(__file__).parent / "outputs"
+
+# Unit conversion
+EV_TO_KJ_PER_MOL = units.mol / units.kJ
+
+
+@pytest.mark.parametrize("mlip", MODELS.items())
+def test_lattice_energy(mlip: tuple[str, Any]) -> None:
+    """
+    Run X23 lattice energy test.
+
+    Parameters
+    ----------
+    mlip
+        Name of model use and model to get calculator.
+    """
+    model_name, model = mlip
+    calc = model.get_calculator()
+
+    # Add D3 calculator for this test
+    calc = model.add_d3_calculator(calc)
+
+    # download X23 dataset
+    lattice_energy_dir = get_benchmark_data("lattice_energy.zip") / "lattice_energy"
+
+    with open(lattice_energy_dir / "list") as f:
+        systems = f.read().splitlines()
+
+    for system in systems:
+        molecule_path = lattice_energy_dir / system / "POSCAR_molecule"
+        solid_path = lattice_energy_dir / system / "POSCAR_solid"
+        ref_path = lattice_energy_dir / system / "lattice_energy_DMC"
+        num_molecules_path = lattice_energy_dir / system / "nmol"
+
+        molecule = read(molecule_path, index=0, format="vasp")
+        molecule.calc = calc
+        molecule.get_potential_energy()
+
+        solid = read(solid_path, index=0, format="vasp")
+        solid.calc = copy(calc)
+        solid.get_potential_energy()
+
+        ref = np.loadtxt(ref_path)[0]
+        num_molecules = np.loadtxt(num_molecules_path)
+
+        solid.info["ref"] = ref
+        solid.info["num_molecules"] = num_molecules
+        solid.info["system"] = system
+        molecule.info["ref"] = ref
+        molecule.info["num_molecules"] = num_molecules
+        molecule.info["system"] = system
+
+        # Write output structures
+        write_dir = OUT_PATH / model_name
+        write_dir.mkdir(parents=True, exist_ok=True)
+        write(write_dir / f"{system}.xyz", [solid, molecule])
