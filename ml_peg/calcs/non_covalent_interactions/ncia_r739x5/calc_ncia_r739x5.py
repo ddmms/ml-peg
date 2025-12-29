26d7f20b158ed52aad4d24374c9b523bb45ae3f5
Domantas Kuryla <dk584@icepc16>
2025-12-29 20:20:17 +0000

    Add D3 calc

--- ml_peg/calcs/non_covalent_interactions/ncia_r739x5/calc_ncia_r739x5.py
+++ ml_peg/calcs/non_covalent_interactions/ncia_r739x5/calc_ncia_r739x5.py
@@ -105,6 +105,8 @@ class NCIAR739x5Benchmark(zntrack.Node):
         self.get_ref_energies(data_path)
 
         calc = self.model.get_calculator()
+        # Add D3 calculator for this test
+        calc = self.model.add_d3_calculator(calc)
 
         for label, ref_energy in tqdm(self.ref_energies.items()):
             xyz_fname = f"{label}.xyz"
