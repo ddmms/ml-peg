4a73d93d5244218f8a2a39ed4702ddff623be0ca
Panos Kourtis (PGR) <c4003490@sage-300737.ncl.ac.uk>
2026-01-16 21:09:23 +0000

    Inter-intra tests operational (parity plots include all models) / Edits to include volume scan tests in the app - still in progress

--- ml_peg/app/bulk_liquids/battery_electrolytes/app_battery_electrolyte.py
+++ ml_peg/app/bulk_liquids/battery_electrolytes/app_battery_electrolyte.py
@@ -8,6 +8,8 @@ from dash.html import Div
 from ml_peg.app import APP_ROOT
 from ml_peg.app.base_app import BaseApp
 from ml_peg.models.get_models import get_model_names
+from ml_peg.app.utils.build_callbacks import plot_from_table_column
+from ml_peg.app.utils.load import read_plot
 from ml_peg.models.models import current_models
 
 MODELS = get_model_names(current_models)
@@ -19,29 +21,35 @@ DATA_PATH = APP_ROOT / "data" / "bulk_liquids" / "battery_electrolyte"
 class BatteryElectrolyteApp(BaseApp):
     """Battery electrolyte benchmark app layout and callbacks."""
 
-    # def register_callbacks(self) -> None:
-    #     """Register callbacks to app."""
-    #     # density_plots = {
-    #     #     model: {
-    #     #         "Bulk modulus MAE": read_density_plot_for_model(
-    #     #             filename=DATA_PATH / "figure_bulk_density.json",
-    #     #             model=model,
-    #     #             id=f"{BENCHMARK_NAME}-{model}-bulk-figure",
-    #     #         ),
-    #     #         "Shear modulus MAE": read_density_plot_for_model(
-    #     #             filename=DATA_PATH / "figure_shear_density.json",
-    #     #             model=model,
-    #     #             id=f"{BENCHMARK_NAME}-{model}-shear-figure",
-    #     #         ),
-    #     #     }
-    #     #     for model in MODELS
-    #     # }
+    def register_callbacks(self) -> None:
+        """Register callbacks to app."""
+        scatter_intraforces = read_plot(
+            DATA_PATH / "intra_forces_parity.json", id=f"{BENCHMARK_NAME}-figure"
+        )
+        scatter_interforces = read_plot(
+            DATA_PATH / "inter_forces_parity.json", id=f"{BENCHMARK_NAME}-figure"
+        )
+        scatter_interenergy = read_plot(
+            DATA_PATH / "inter_energy_parity.json", id=f"{BENCHMARK_NAME}-figure"
+        )
+        scatter_intravirial = read_plot(
+            DATA_PATH / "intra_virial_parity.json", id=f"{BENCHMARK_NAME}-figure"
+        )
+        scatter_intervirial = read_plot(
+            DATA_PATH / "inter_virial_parity.json", id=f"{BENCHMARK_NAME}-figure"
+        )
 
-    #     plot_from_table_cell(
-    #         table_id=self.table_id,
-    #         plot_id=f"{BENCHMARK_NAME}-figure-placeholder",
-    #         cell_to_plot=density_plots,
-    #     )
+        plot_from_table_column(
+            table_id=self.table_id,
+            plot_id=f"{BENCHMARK_NAME}-figure-placeholder",
+            column_to_plot={
+                "Intra-Forces": scatter_intraforces,
+                "Inter-Forces": scatter_interforces,
+                "Inter-Energy": scatter_interenergy,
+                "Intra-Virial": scatter_intravirial,
+               "Inter-Virial": scatter_intervirial
+            },
+        )
 
 
 def get_app() -> BatteryElectrolyteApp:
@@ -56,9 +64,9 @@ def get_app() -> BatteryElectrolyteApp:
     return BatteryElectrolyteApp(
         name=BENCHMARK_NAME,
         description=(
-            "Evaluate model inter/intra property prediction"
+            "Evaluate model inter/intra property prediction "
             "for different densities of LIB full electrolyte"
-            "and neat solvent configs"
+            " and neat solvent configs"
         ),
         # docs_url=DOCS_URL,
         table_path=DATA_PATH / "Inter_intra_metrics_table.json",
@@ -72,5 +80,5 @@ if __name__ == "__main__":
     full_app = Dash(__name__, assets_folder=DATA_PATH.parent.parent)
     BatteryElectrolyte_app = get_app()
     full_app.layout = BatteryElectrolyte_app.layout
-    # BatteryElectrolyte_app.register_callbacks()
+    BatteryElectrolyte_app.register_callbacks()
     full_app.run(port=8054, debug=True)
