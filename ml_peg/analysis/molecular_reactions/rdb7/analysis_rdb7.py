19e1ade476fbe8b85a01551f6c79295eed84d1b7
Domantas Kuryla <dk584@icepc16>
2026-01-19 16:58:28 +0000

    Add RDB7 Analysis

---
+++ ml_peg/analysis/molecular_reactions/rdb7/analysis_rdb7.py
@@ -0,0 +1,143 @@
+"""
+Analyse the RDB7 reaction barrier dataset.
+
+Spiekermann, K., Pattanaik, L. & Green, W.H.
+High accuracy barrier heights, enthalpies,
+and rate coefficients for chemical reactions.
+Sci Data 9, 417 (2022)
+https://doi.org/10.1038/s41597-022-01529-6
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from ase import units
+from ase.io import read, write
+import pytest
+
+from ml_peg.analysis.utils.decorators import build_table, plot_parity
+from ml_peg.analysis.utils.utils import build_d3_name_map, load_metrics_config, mae
+from ml_peg.app import APP_ROOT
+from ml_peg.calcs import CALCS_ROOT
+from ml_peg.models.get_models import load_models
+from ml_peg.models.models import current_models
+
+MODELS = load_models(current_models)
+D3_MODEL_NAMES = build_d3_name_map(MODELS)
+
+KCAL_TO_EV = units.kcal / units.mol
+EV_TO_KCAL = 1 / KCAL_TO_EV
+CALC_PATH = CALCS_ROOT / "molecular_reactions" / "rdb7" / "outputs"
+OUT_PATH = APP_ROOT / "data" / "molecular_reactions" / "rdb7"
+
+METRICS_CONFIG_PATH = Path(__file__).with_name("metrics.yml")
+DEFAULT_THRESHOLDS, DEFAULT_TOOLTIPS, DEFAULT_WEIGHTS = load_metrics_config(
+    METRICS_CONFIG_PATH
+)
+
+
+def labels() -> list:
+    """
+    Get list of system names.
+
+    Returns
+    -------
+    list
+        List of all system names.
+    """
+    return [str(i).zfill(6) for i in range(0, 11961)]
+
+
+@pytest.fixture
+@plot_parity(
+    filename=OUT_PATH / "figure_rdb7_barriers.json",
+    title="Reaction barriers",
+    x_label="Predicted barrier / kcal/mol",
+    y_label="Reference barrier / kcal/mol",
+    hoverdata={
+        "Labels": labels(),
+    },
+)
+def barrier_heights() -> dict[str, list]:
+    """
+    Get barrier heights for all systems.
+
+    Returns
+    -------
+    dict[str, list]
+        Dictionary of all reference and predicted barrier heights.
+    """
+    results = {"ref": []} | {mlip: [] for mlip in MODELS}
+    ref_stored = False
+
+    for model_name in MODELS:
+        for label in labels():
+            atoms = read(CALC_PATH / model_name / f"{label}_ts.xyz")
+            results[model_name].append(atoms.info["model_forward_barrier"] * EV_TO_KCAL)
+            if not ref_stored:
+                results["ref"].append(atoms.info["ref_forward_barrier"] * EV_TO_KCAL)
+
+            # Write structures for app
+            structs_dir = OUT_PATH / model_name
+            structs_dir.mkdir(parents=True, exist_ok=True)
+            write(structs_dir / f"{label}_ts.xyz", atoms)
+        ref_stored = True
+    return results
+
+
+@pytest.fixture
+def get_mae(barrier_heights) -> dict[str, float]:
+    """
+    Get mean absolute error for barrier heights.
+
+    Parameters
+    ----------
+    barrier_heights
+        Dictionary of reference and predicted barrier heights.
+
+    Returns
+    -------
+    dict[str, float]
+        Dictionary of predicted barrier height errors for all models.
+    """
+    results = {}
+    for model_name in MODELS:
+        results[model_name] = mae(barrier_heights["ref"], barrier_heights[model_name])
+    return results
+
+
+@pytest.fixture
+@build_table(
+    filename=OUT_PATH / "rdb7_barriers_metrics_table.json",
+    metric_tooltips=DEFAULT_TOOLTIPS,
+    thresholds=DEFAULT_THRESHOLDS,
+    mlip_name_map=D3_MODEL_NAMES,
+)
+def metrics(get_mae: dict[str, float]) -> dict[str, dict]:
+    """
+    Get all metrics.
+
+    Parameters
+    ----------
+    get_mae
+        Mean absolute errors for all models.
+
+    Returns
+    -------
+    dict[str, dict]
+        Metric names and values for all models.
+    """
+    return {"MAE": get_mae}
+
+
+def test_rdb7_barriers(metrics: dict[str, dict]) -> None:
+    """
+    Run rdb7_barriers test.
+
+    Parameters
+    ----------
+    metrics
+        All new benchmark metric names and dictionary of values for each model.
+    """
+    return
