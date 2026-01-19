094f30ab30ec923b8d482c94addcdaa332cb8c63
Panos Kourtis (PGR) <c4003490@sage-300737.ncl.ac.uk>
2026-01-19 16:48:25 +0000

    Finalised Volume scan analysis, remaining issue is configs do not show on the app

--- ml_peg/analysis/bulk_liquids/battery_electrolyte/analyse_battery_electrolyte.py
+++ ml_peg/analysis/bulk_liquids/battery_electrolyte/analyse_battery_electrolyte.py
@@ -5,10 +5,11 @@ from __future__ import annotations
 from pathlib import Path
 
 from ase.io import read
+from aseMolec import extAtoms as ea
 import numpy as np
 import pytest
-from aseMolec import extAtoms as ea
-from ml_peg.analysis.utils.decorators import build_table, plot_parity
+
+from ml_peg.analysis.utils.decorators import build_table, plot_parity, plot_scatter
 from ml_peg.analysis.utils.utils import load_metrics_config, rmse
 from ml_peg.app import APP_ROOT
 from ml_peg.calcs import CALCS_ROOT
@@ -170,40 +171,80 @@ def test_rmse_metrics(
     return
 
 
-@pytest.fixture
-def get_volscan_results(conf_type: str) -> tuple[dict[str, list[float]], dict[str, list[float]]]:
+
+def get_volscan_results(
+    conf_type: str,
+) -> tuple[dict[str, list[float]], dict[str, list[float]]]:
     """
     Get relative energies per atom for a type of Volume Scan.
 
     Returns
     -------
     results
-        Relative energy of each model per Volume Scan config.
-    densities
-        Density of each volume scan config
+        Relative energy of each model per Volume Scan config density.
     """
     results = {"ref": []} | {mlip: [] for mlip in MODELS}
     densities = {"ref": []} | {mlip: [] for mlip in MODELS}
 
     for model in results.keys():
         if model == "ref":
-            configs = read(REF_PATH / "Volume_Scan_data" /
-                              f"{type}_VS_PBED3.xyz", ":")
+            configs = read(REF_PATH / "Volume_Scan_data" / f"{conf_type}_VS_PBED3.xyz", ":")
 
         else:
             configs = read(
-                CALC_PATH / "Volume_Scan_output" / f"{type}_VS_{model}_D3.xyz", ":"
+                CALC_PATH / "Volume_Scan_output" / f"{conf_type}_VS_{model}_D3.xyz", ":"
             )
 
         energies = [
-            frame.calc.__dict__["results"]["energy"] / len(frame) for frame in configs
+            frame.calc.__dict__["results"]["energy"]*1000 / len(frame) for frame in configs
         ]
-        relative_energies = energies - min(energies)
-
-        results[model].append(relative_energies.tolist())
+        relative_energies = (energies - min(energies))
         densities = [ea.get_density_gcm3(frame) for frame in configs]
+        
 
-    return results, densities
+        results[model].append(densities)
+        results[model].append(relative_energies.tolist())
+
+    return results
+
+
+def plot_volscans(conf_key: str, model: str, results: dict[str, float]) -> None:
+    """
+    Plot Volume Scan scatter plots.
+
+    Parameters
+    ----------
+    conf_key
+        Name of Volume Scan type to be plotted.
+    model
+        Name of MLIP.
+    results
+        Results from all models for a single Volume Scan.
+    """
+
+    @plot_scatter(
+        filename=OUT_PATH / f"{conf_key}_{model}_volscan_scatter.json",
+        title=f"{conf_key} Volume Scan",
+        x_label="Density / g cm<sup>-3</sup>",
+        y_label="Energy wrt Minimum Energy / meV/atom",
+        show_line=True,
+    )
+    def plot_result() -> dict[str, list[float]]:
+        """
+        Plot the Volume Scan plots.
+
+        Returns
+        -------
+        model_results
+            Dictionary of reference and a specific MLIP's Volume Scan energies.
+        """
+        model_results = {
+            "ref" : results["ref"],
+            model : results[model]
+        }
+        return model_results
+    plot_result()
+
 
 @pytest.fixture
 def get_volscan_rmses() -> dict[str, dict]:
@@ -218,110 +259,47 @@ def get_volscan_rmses() -> dict[str, dict]:
     volscan_rmse = {conf_type: {} for conf_type in conf_types}
 
     for conf_type in conf_types:
-        results, densities = get_volscan_results(conf_type)
-        #plot_results(prop_key, results)
+        results = get_volscan_results(conf_type)
         for model in MODELS:
-            model_rmse = rmse(results["ref"], results[model])
+            model_rmse = rmse(results["ref"][1], results[model][1])
             volscan_rmse[conf_type][model] = model_rmse
+            plot_volscans(conf_type, model, results)
 
     return volscan_rmse
 
-# @pytest.fixture
-# @build_table(
-#     filename=OUT_PATH / "Volscan_metrics_table.json",
-#     metric_tooltips=DEFAULT_TOOLTIPS,
-#     thresholds=DEFAULT_THRESHOLDS,
-# )
-# def rmse_metrics(get_property_rmses: dict[str, dict]) -> dict[str, dict]:
-#     """
-#     Get all inter intra RMSE metrics.
+@pytest.fixture
+@build_table(
+    filename=OUT_PATH / "Vol_scan_rmses_table.json",
+    metric_tooltips=DEFAULT_TOOLTIPS,
+    thresholds=DEFAULT_THRESHOLDS,
+)
+def vs_rmse_metrics(get_volscan_rmses: dict[str, dict]) -> dict[str, dict]:
+    """
+    Get all Volume Scan RMSE metrics.
 
-#     Parameters
-#     ----------
-#     get_property_rmses
-#         Dictionary for every property containing each model's RMSE.
+    Parameters
+    ----------
+    get_property_rmses
+        Dictionary of each model's RMSE for all Volume Scans.
 
-#     Returns
-#     -------
-#     dict[str, dict]
-#         Dictionary for every property containing each model's RMSE.
-#     """
-#     return get_property_rmses
-
-
-# def test_rmse_metrics(
-#     rmse_metrics: dict[str, dict],
-# ) -> None:
-#     """
-#     Run inter-intra property test.
-
-#     Parameters
-#     ----------
-#     rmse_metrics
-#         All inter-intra metrics.
-#     """
-#     return
+    Returns
+    -------
+    dict[str, dict]
+        Dictionary of each model's RMSE for all Volume Scans.
+    """
+    return get_volscan_rmses
 
 
-# @pytest.fixture
-# @plot_parity(
-#     filename=OUT_PATH / "solvent_volscan_scatter.json",
-#     title="Solvent Volume Scans",
-#     x_label="Predicted intra-forces / eV/atom",
-#     y_label="DFT intra-forces / eV/atom",
-# )
-# def solvent_volscan_scatter() -> dict[str, float]:
-#     """
-#     Solvent volume scan results of each model for parity plots.
+def test_vs_rmse_metrics(
+    vs_rmse_metrics: dict[str, dict],
+) -> None:
+    """
+    Run Volume Scans test.
 
-#     Returns
-#     -------
-#     tuple[dict[str, float], dict[str, float]]
-#         Volume scan energies and densities for each model.
-#     """
-#     solvent_vs_energies, solvent_vs_densities = volscan_results("solvent")
-#     return solvent_vs_energies, solvent_vs_densities
+    Parameters
+    ----------
+    vs_rmse_metrics
+        All Volume Scan metrics.
+    """
+    return
 
-
-# def plot_volscans(model: str) -> None:
-#     """
-#     Plot volume scans and save all structure files.
-
-#     Parameters
-#     ----------
-#     model
-#         Name of MLIP.
-#     """
-
-#     @plot_scatter(
-#         filename=OUT_PATH / f"{conf_type}_VS_{model}.json",
-#         title=f"Volume Scan of {conf_type} configs",
-#         x_label="Density / g/cm3",
-#         y_label="Energy / eV/atom",
-#         show_line=False,
-#     )
-#     def plot_volscan() -> dict[str, tuple[list[float], list[float]]]:
-#         """
-#         Plot a Volume Scan and save the structure file.
-
-#         Returns
-#         -------
-#         dict[str, tuple[list[float], list[float]]]
-#             Dictionary of tuples of image/energy for each model.
-#         """
-#         results = {}
-#         structs = read(
-#             CALC_PATH / f"li_diffusion_{path.lower()}-{model}-neb-band.extxyz",
-#             index=":",
-#         )
-#         results[model] = [
-#             list(range(len(structs))),
-#             [struct.get_potential_energy() for struct in structs],
-#         ]
-#         structs_dir = OUT_PATH / model
-#         structs_dir.mkdir(parents=True, exist_ok=True)
-#         write(structs_dir / f"{model}-{path.lower()}-neb-band.extxyz", structs)
-
-#         return results
-
-#     plot_neb()
--- ml_peg/app/bulk_liquids/battery_electrolytes/app_battery_electrolyte.py
+++ ml_peg/app/bulk_liquids/battery_electrolytes/app_battery_electrolyte.py
@@ -6,8 +6,12 @@ from dash import Dash
 from dash.html import Div
 
 from ml_peg.app import APP_ROOT
+from ml_peg.calcs import CALCS_ROOT
 from ml_peg.app.base_app import BaseApp
-from ml_peg.app.utils.build_callbacks import plot_from_table_cell
+from ml_peg.app.utils.build_callbacks import (
+    plot_from_table_cell,
+    struct_from_scatter,
+)
 from ml_peg.app.utils.load import read_plot
 from ml_peg.models.get_models import get_model_names
 from ml_peg.models.models import current_models
@@ -17,6 +21,7 @@ MODELS = MODELS[:-1]
 
 BENCHMARK_NAME = "Battery electrolyte"
 DATA_PATH = APP_ROOT / "data" / "bulk_liquids" / "battery_electrolyte"
+REF_PATH = CALCS_ROOT / "bulk_liquids" / "battery_electrolyte" / "data"
 
 
 class BatteryElectrolyteApp(BaseApp):
@@ -24,37 +29,72 @@ class BatteryElectrolyteApp(BaseApp):
 
     def register_callbacks(self) -> None:
         """Register callbacks to app."""
-        parity_plots = {
+        # parity_plots = {
+        #     model: {
+        #         "Intra-Forces": read_plot(
+        #             DATA_PATH / f"intra_forces_parity_{model}.json",
+        #             id=f"{BENCHMARK_NAME}-{model}-figure",
+        #         ),
+        #         "Inter-Forces": read_plot(
+        #             DATA_PATH / f"inter_forces_parity_{model}.json",
+        #             id=f"{BENCHMARK_NAME}-{model}-figure",
+        #         ),
+        #         "Inter-Energy": read_plot(
+        #             DATA_PATH / f"inter_energy_parity_{model}.json",
+        #             id=f"{BENCHMARK_NAME}-{model}-figure",
+        #         ),
+        #         "Intra-Virial": read_plot(
+        #             DATA_PATH / f"intra_virial_parity_{model}.json",
+        #             id=f"{BENCHMARK_NAME}-{model}-figure",
+        #         ),
+        #         "Inter-Virial": read_plot(
+        #             DATA_PATH / f"inter_virial_parity_{model}.json",
+        #             id=f"{BENCHMARK_NAME}-{model}-figure",
+        #         ),
+        #     }
+        #     for model in MODELS
+        # }
+
+        # plot_from_table_cell(
+        #     table_id=self.table_id,
+        #     plot_id=f"{BENCHMARK_NAME}-figure-placeholder",
+        #     cell_to_plot=parity_plots,
+        # )
+        scatter_plots = {
             model: {
-                "Intra-Forces": read_plot(
-                    DATA_PATH / f"intra_forces_parity_{model}.json",
-                    id=f"{BENCHMARK_NAME}-{model}-figure",
+                "solvent": read_plot(
+                    DATA_PATH / f"solvent_{model}_volscan_scatter.json",
+                    id=f"{BENCHMARK_NAME}-{model}-figure-solventVS",
                 ),
-                "Inter-Forces": read_plot(
-                    DATA_PATH / f"inter_forces_parity_{model}.json",
-                    id=f"{BENCHMARK_NAME}-{model}-figure",
-                ),
-                "Inter-Energy": read_plot(
-                    DATA_PATH / f"inter_energy_parity_{model}.json",
-                    id=f"{BENCHMARK_NAME}-{model}-figure",
-                ),
-                "Intra-Virial": read_plot(
-                    DATA_PATH / f"intra_virial_parity_{model}.json",
-                    id=f"{BENCHMARK_NAME}-{model}-figure",
-                ),
-                "Inter-Virial": read_plot(
-                    DATA_PATH / f"inter_virial_parity_{model}.json",
-                    id=f"{BENCHMARK_NAME}-{model}-figure",
+                "electrolyte": read_plot(
+                    DATA_PATH / f"electrolyte_{model}_volscan_scatter.json",
+                    id=f"{BENCHMARK_NAME}-{model}-figure-electrolyteVS",
                 ),
             }
             for model in MODELS
         }
 
+        # Assets dir will be parent directory
+        ref_conf_dir = REF_PATH / "Volume_Scan_data"
+        structs = {
+                "solvent": f"{ref_conf_dir}/solvent_VS_PBED3.extxyz",
+                "electrolyte": f"{ref_conf_dir}/electrolyte_VS_PBED3.extxyz",
+            }
+
         plot_from_table_cell(
             table_id=self.table_id,
             plot_id=f"{BENCHMARK_NAME}-figure-placeholder",
-            cell_to_plot=parity_plots,
+            cell_to_plot=scatter_plots,
         )
+
+        for model in MODELS:
+            for volscan in ("solvent", "electrolyte"):
+                struct_from_scatter(
+                    scatter_id=f"{BENCHMARK_NAME}-{model}-figure-{volscan}VS",
+                    struct_id=f"{BENCHMARK_NAME}-struct-placeholder",
+                    structs=structs[volscan],
+                    mode="traj",
+                )
 
 
 def get_app() -> BatteryElectrolyteApp:
@@ -74,9 +114,14 @@ def get_app() -> BatteryElectrolyteApp:
             " and neat solvent configs"
         ),
         # docs_url=DOCS_URL,
-        table_path=DATA_PATH / "Inter_intra_metrics_table.json",
+        # table_path=DATA_PATH / "Inter_intra_metrics_table.json",
+        # extra_components=[
+        #     Div(id=f"{BENCHMARK_NAME}-figure-placeholder"),
+        # ],
+        table_path=DATA_PATH / "Vol_scan_rmses_table.json",
         extra_components=[
             Div(id=f"{BENCHMARK_NAME}-figure-placeholder"),
+            Div(id=f"{BENCHMARK_NAME}-struct-placeholder"),
         ],
     )
 
