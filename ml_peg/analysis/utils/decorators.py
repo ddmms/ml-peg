3b5fe279b75fea15e24ec8fc84309987c314736d
Joseph Hart <92541539+joehart2001@users.noreply.github.com>
2026-01-22 17:06:46 +0000

    Update ml_peg/analysis/utils/decorators.py
    Co-authored-by: Elliott Kasoar <45317199+ElliottKasoar@users.noreply.github.com>

--- ml_peg/analysis/utils/decorators.py
+++ ml_peg/analysis/utils/decorators.py
@@ -1397,7 +1397,7 @@ def build_table(
             metrics_columns = ("MLIP",) + tuple(results)
 
             # Get all models (including those without results for this benchmark)
-            mlips = tuple(get_model_names(None))
+            mlips = tuple(get_model_names())
 
             name_map = mlip_name_map if mlip_name_map else {}
             display_names = {mlip: name_map.get(mlip, mlip) for mlip in mlips}
