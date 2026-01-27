9ee5b85e340ca7846aa36179c5d805694fdb7ef4
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2026-01-27 13:01:35 +0000

    Update uv.lock on release

--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@ version=$1
 sed -i.bak "s;^version =.*;version = \"$version\";g" pyproject.toml && rm pyproject.toml.bak
 
 git add pyproject.toml
+uv sync
+git add uv.lock
 git commit -m "Bump version for release $version"
 git tag -f -a v$version -m "Release $version"
 git push
