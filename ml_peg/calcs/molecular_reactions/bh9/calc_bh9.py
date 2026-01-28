6410afb4e59a169ba86bbffab553d34dce301080
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2026-01-28 16:35:40 +0000

    Remove mlipx

--- ml_peg/calcs/molecular_reactions/bh9/calc_bh9.py
+++ ml_peg/calcs/molecular_reactions/bh9/calc_bh9.py
@@ -8,15 +8,14 @@ DOI: 10.1021/acs.jctc.1c00694
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Any
 
 from ase import units
 from ase.io import read, write
-import mlipx
-from mlipx.abc import NodeWithCalculator
+import pytest
 from tqdm import tqdm
-import zntrack
 
-from ml_peg.calcs.utils.utils import chdir, download_s3_data
+from ml_peg.calcs.utils.utils import download_s3_data
 from ml_peg.models.get_models import load_models
 from ml_peg.models.models import current_models
 
@@ -77,102 +76,84 @@ def parse_cc_energy(fname):
     return float(items[1]) * KCAL_TO_EV
 
 
-class BH9Benchmark(zntrack.Node):
-    """Benchmark the BH9 reaction benchmark."""
-
-    model: NodeWithCalculator = zntrack.deps()
-    model_name: str = zntrack.params()
-
-    def get_ref_energies(self, data_path):
-        """
-        Get the reference barriers.
-
-        Parameters
-        ----------
-        data_path
-            Path to the dataset directory.
-        """
-        self.ref_energies = {}
-        labels = [
-            path.stem.replace("TS", "")
-            for path in sorted((data_path / "BH9_SI" / "XYZ_files").glob("*TS.xyz"))
-        ]
-        rxn_count = 0
-        for label in labels:
-            self.ref_energies[label] = {}
-            rxn_count += 1
-            for direction in ["forward", "reverse"]:
-                ref_fname = (
-                    data_path
-                    / "BH9_SI"
-                    / "DB_files"
-                    / "BH"
-                    / f"BH9-BH_{rxn_count}_{direction}.db"
-                )
-                self.ref_energies[label][direction] = parse_cc_energy(ref_fname)
-
-    def run(self):
-        """Run new benchmark."""
-        data_path = (
-            download_s3_data(
-                filename="BH9.zip",
-                key="inputs/molecular_reactions/BH9/BH9.zip",
-            )
-            / "BH9"
-        )
-        # Read in data and attach calculator
-        self.get_ref_energies(data_path)
-        calc = self.model.get_calculator()
-        # Add D3 calculator for this test
-        calc = self.model.add_d3_calculator(calc)
-
-        for fname in tqdm(sorted((data_path / "BH9_SI" / "XYZ_files").glob("*TS.xyz"))):
-            atoms = process_atoms(fname)
-            atoms.calc = calc
-            atoms.info["model_energy"] = atoms.get_potential_energy()
-
-            """
-            Write both forward and reverse barriers,
-            only forward will be used in analysis here.
-            """
-            label = fname.stem
-            if "TS" in label:
-                label = label.replace("TS", "")
-                atoms.info["ref_forward_barrier"] = self.ref_energies[label]["forward"]
-                atoms.info["ref_reverse_barrier"] = self.ref_energies[label]["reverse"]
-
-            write_dir = OUT_PATH / self.model_name
-            write_dir.mkdir(parents=True, exist_ok=True)
-            write(write_dir / f"{fname.stem}.xyz", atoms)
-
-
-def build_project(repro: bool = False) -> None:
+def get_ref_energies(data_path: Path) -> dict[str, dict[str, float]]:
     """
-    Build mlipx project.
+    Get the reference barriers.
 
     Parameters
     ----------
-    repro
-        Whether to call dvc repro -f after building.
+    data_path
+        Path to the dataset directory.
+
+    Returns
+    -------
+    dict[str, dict[str, float]]
+        Loaded reference energies.
     """
-    project = mlipx.Project()
-    benchmark_node_dict = {}
+    ref_energies = {}
+    labels = [
+        path.stem.replace("TS", "")
+        for path in sorted((data_path / "BH9_SI" / "XYZ_files").glob("*TS.xyz"))
+    ]
+    rxn_count = 0
 
-    for model_name, model in MODELS.items():
-        with project.group(model_name):
-            benchmark = BH9Benchmark(
-                model=model,
-                model_name=model_name,
+    for label in labels:
+        ref_energies[label] = {}
+        rxn_count += 1
+        for direction in ["forward", "reverse"]:
+            ref_fname = (
+                data_path
+                / "BH9_SI"
+                / "DB_files"
+                / "BH"
+                / f"BH9-BH_{rxn_count}_{direction}.db"
             )
-            benchmark_node_dict[model_name] = benchmark
+            ref_energies[label][direction] = parse_cc_energy(ref_fname)
 
-    if repro:
-        with chdir(Path(__file__).parent):
-            project.repro(build=True, force=True)
-    else:
-        project.build()
+    return ref_energies
 
 
-def test_bh9_barrier_heights():
-    """Run BH9 barriers benchmark via pytest."""
-    build_project(repro=True)
+@pytest.mark.parametrize("mlip", MODELS.items())
+def test_bh9(mlip: tuple[str, Any]) -> None:
+    """
+    Run BH9 benchmark.
+
+    Parameters
+    ----------
+    mlip
+        Name of model use and model to get calculator.
+    """
+    model_name, model = mlip
+    calc = model.get_calculator()
+
+    data_path = (
+        download_s3_data(
+            filename="BH9.zip",
+            key="inputs/molecular_reactions/BH9/BH9.zip",
+        )
+        / "BH9"
+    )
+    # Read in data and attach calculator
+    ref_energies = get_ref_energies(data_path)
+    calc = model.get_calculator()
+    # Add D3 calculator for this test
+    calc = model.add_d3_calculator(calc)
+
+    for fname in tqdm(sorted((data_path / "BH9_SI" / "XYZ_files").glob("*TS.xyz"))):
+        atoms = process_atoms(fname)
+        atoms.calc = calc
+        atoms.info["model_energy"] = atoms.get_potential_energy()
+
+        """
+        Write both forward and reverse barriers,
+        only forward will be used in analysis here.
+        """
+        label = fname.stem
+        if "TS" in label:
+            label = label.replace("TS", "")
+            atoms.info["ref_forward_barrier"] = ref_energies[label]["forward"]
+            atoms.info["ref_reverse_barrier"] = ref_energies[label]["reverse"]
+
+        write_dir = OUT_PATH / model_name
+        write_dir.mkdir(parents=True, exist_ok=True)
+        write(write_dir / f"{fname.stem}.xyz", atoms)
