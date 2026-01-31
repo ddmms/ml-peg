bc4d742b0b361ec741062762843b3b94ad0ba05c
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2026-01-31 19:30:12 +0000

    Update dir names

--- ml_peg/app/non_covalent_interactions/NCIA_IHB100x10/app_NCIA_IHB100x10.py
+++ ml_peg/app/non_covalent_interactions/NCIA_IHB100x10/app_NCIA_IHB100x10.py
@@ -1,4 +1,4 @@
-"""Run NCIA_IHB100x10 app."""
+"""Run NCIA IHB100x10 app."""
 
 from __future__ import annotations
 
@@ -16,16 +16,16 @@ from ml_peg.models.get_models import get_model_names
 from ml_peg.models.models import current_models
 
 MODELS = get_model_names(current_models)
-BENCHMARK_NAME = "NCIA_IHB100x10"
+BENCHMARK_NAME = "NCIA IHB100x10"
 DOCS_URL = (
     "https://ddmms.github.io/ml-peg/user_guide/benchmarks/"
     "non_covalent_interactions.html#ncia-ihb100x10"
 )
-DATA_PATH = APP_ROOT / "data" / "non_covalent_interactions" / "ncia_ihb100x10"
+DATA_PATH = APP_ROOT / "data" / "non_covalent_interactions" / "NCIA_IHB100x10"
 
 
 class NCIANIHB100x10App(BaseApp):
-    """NCIA_IHB100x10 benchmark app layout and callbacks."""
+    """NCIA IHB100x10 benchmark app layout and callbacks."""
 
     def register_callbacks(self) -> None:
         """Register callbacks to app."""
@@ -38,7 +38,7 @@ class NCIANIHB100x10App(BaseApp):
         if model_dir.exists():
             labels = sorted([f.stem for f in model_dir.glob("*.xyz")])
             structs = [
-                f"assets/non_covalent_interactions/ncia_ihb100x10/{MODELS[0]}/{label}.xyz"
+                f"assets/non_covalent_interactions/NCIA_IHB100x10/{MODELS[0]}/{label}.xyz"
                 for label in labels
             ]
         else:
