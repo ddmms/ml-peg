cd6f081771433e90a890c2ab4e8cea88318cd624
Panos Kourtis (PGR) <c4003490@sage-300737.ncl.ac.uk>
2026-02-05 22:09:25 +0000

    Test calc script for aqueous iron chloride MD

--- ml_peg/calcs/physicality/oxidation_states/calc_oxidation_states.py
+++ ml_peg/calcs/physicality/oxidation_states/calc_oxidation_states.py
@@ -5,12 +5,8 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Any
 
-from ase import Atoms, units
 from ase.io import read
-from janus_core.calculations.geom_opt import GeomOpt
 from janus_core.calculations.md import NPT
-from janus_core.calculations.single_point import SinglePoint
-import numpy as np
 import pytest
 
 from ml_peg.models.get_models import load_models
@@ -21,7 +17,8 @@ MODELS = load_models(current_models)
 DATA_PATH = Path(__file__).parent / "data"
 OUT_PATH = Path(__file__).parent / "outputs"
 
-IRON_SALTS = ["Fe2Cl, Fe3Cl"]
+IRON_SALTS = ["Fe2Cl", "Fe3Cl"]
+
 
 @pytest.mark.slow
 @pytest.mark.parametrize("mlip", MODELS.items())
@@ -34,54 +31,31 @@ def test_iron_oxidation_state_md(mlip: tuple[str, Any]) -> None:
     mlip
         Name of model use and model to get calculator.
     """
+    model_name, model = mlip
+    calc = model.get_calculator()
 
+    # Add D3 calculator for this test
+    calc = model.add_d3_calculator(calc)
 
     for salt in IRON_SALTS:
         struct_path = DATA_PATH / f"{salt}_start.xyz"
         struct = read(struct_path, "0")
+        struct.calc = calc
 
         npt = NPT(
             struct=struct,
-            steps=40000,
+            steps=200,
             timestep=0.5,
-            stats_every=50,
-            traj_every=50,
+            stats_every=100,
+            traj_every=200,
             traj_append=True,
             thermostat_time=50,
             bulk_modulus=10,
-            barostat_time=1500,
-            pressure=pressure,
-            file_prefix=OUT_PATH / f"{salt}_{mlip}",
-            restart=True,
-            restart_auto=False,
+            barostat_time=None,
+            # pressure=pressure,
+            file_prefix=OUT_PATH / f"{salt}_{model_name}",
+            # restart=True,
+            # restart_auto=True,
+            # post_process_kwargs={"rdf_compute": True, "rdf_rmax": 6, "rdf_bins": 120},
         )
         npt.run()
-
-
-@pytest.mark.parametrize("mlip", MODELS.items())
-def test_static_md(mlip: tuple[str, Any]) -> None:
-    """
-    Evaluate an existing AIMD trajectory for the static high-pressure hydrogen tests.
-
-    Parameters
-    ----------
-    mlip
-        Name of model use and model to get calculator.
-    """
-    struct_path = DATA_PATH / "Hpres.xyz"
-
-    model_name, model = mlip
-    calc = model.get_calculator()
-
-    structs = read(struct_path, index="::50")
-    structs.calc = calc
-    for struct in structs:
-        struct.info["density"] = (
-            np.sum(struct.get_masses()) / struct.get_volume() * DENS_FACT
-        )
-
-    SinglePoint(
-        struct=structs,
-        write_results=True,
-        file_prefix=OUT_PATH / f"H-static-{mlip}",
-    ).run()
