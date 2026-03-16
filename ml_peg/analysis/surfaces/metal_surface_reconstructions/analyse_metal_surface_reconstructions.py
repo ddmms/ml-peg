e78d38971d7058b2e17ecaa4dae2704581928707
FelixRccs <felix.r@go4more.de>
2026-03-16 11:26:08 +0000

    check formatting

--- ml_peg/analysis/surfaces/metal_surface_reconstructions/analyse_metal_surface_reconstructions.py
+++ ml_peg/analysis/surfaces/metal_surface_reconstructions/analyse_metal_surface_reconstructions.py
@@ -1,3 +1,4 @@
+"""Analyse Metal_Surface_Reconstructions benchmark."""
 
 from __future__ import annotations
 
@@ -9,7 +10,7 @@ import numpy as np
 import pytest
 
 from ml_peg.analysis.utils.decorators import build_table, plot_parity
-from ml_peg.analysis.utils.utils import build_d3_name_map, load_metrics_config, mae
+from ml_peg.analysis.utils.utils import load_metrics_config, mae
 from ml_peg.app import APP_ROOT
 from ml_peg.calcs import CALCS_ROOT
 from ml_peg.models.get_models import get_model_names
@@ -17,7 +18,7 @@ from ml_peg.models.models import current_models
 
 MODELS = get_model_names(current_models)
 print(MODELS)
-#D3_MODEL_NAMES = build_d3_name_map(MODELS)
+
 CALC_PATH = CALCS_ROOT / "surfaces" / "metal_surface_reconstructions" / "outputs"
 OUT_PATH = APP_ROOT / "data" / "surfaces" / "metal_surfaces"
 
@@ -32,7 +33,7 @@ EV_TO_KJ_PER_MOL = units.mol / units.kJ
 
 def get_system_names() -> list[str]:
     """
-    Get list of metal surface system names.
+    Get list of metal surface reconstructions system names.
 
     Returns
     -------
@@ -44,30 +45,29 @@ def get_system_names() -> list[str]:
         model_dir = CALC_PATH / model_name
         print(model_dir)
         if model_dir.exists():
-            print('yes')
+            print("yes")
             xyz_files = sorted(model_dir.glob("*.xyz"))
             if xyz_files:
                 for xyz_file in xyz_files:
                     atoms = read(xyz_file)
                     system_names.append(atoms.info["system"])
-                
-    return system_names
 
+    return system_names
 
 
 @pytest.fixture
 @plot_parity(
     filename=OUT_PATH / "slab_energies.json",
     title="Metal Slab Energies",
-    x_label="Predicted Surface Free Energy / meV/Å²",
-    y_label="Reference EneSurface Free Energy / meV/Å²",
+    x_label="Predicted Surface Energy / meV/Å²",
+    y_label="Reference Surface Energy / meV/Å²",
     hoverdata={
         "System": get_system_names(),
     },
 )
 def slab_energies() -> dict[str, list]:
     """
-    Get energies for all slabs systems.
+    Get surface energies for all slabs systems.
 
     Returns
     -------
@@ -88,30 +88,37 @@ def slab_energies() -> dict[str, list]:
         xyz_files = sorted(model_dir.glob("*.xyz"))
         if not xyz_files:
             continue
-        
+
         model_mu = {}
         for xyz_file in xyz_files:
             name = xyz_file.name
-            if name.startswith('bulk') or name.startswith('gas_phase'):
+            if name.startswith("bulk") or name.startswith("gas_phase"):
                 structs = read(xyz_file, index=":")[0]
-                model_mu[structs.get_chemical_symbols()[0]] = structs.get_potential_energy()/len(structs)
+                model_mu[structs.get_chemical_symbols()[0]] = (
+                    structs.get_potential_energy() / len(structs)
+                )
                 if not ref_stored:
-                    ref_mu[structs.get_chemical_symbols()[0]] = structs.info['DFT_energy']/len(structs)
-
-
+                    ref_mu[structs.get_chemical_symbols()[0]] = structs.info[
+                        "DFT_energy"
+                    ] / len(structs)
 
         for xyz_file in xyz_files:
             name = xyz_file.name
-            if not (name.startswith('bulk') or name.startswith('gas_phase')):
+            if not (name.startswith("bulk") or name.startswith("gas_phase")):
                 structs = read(xyz_file, index=":")[0]
                 system = structs.info["system"]
                 symbols = structs.get_chemical_symbols()
                 cell = structs.cell
-                A = np.linalg.norm(np.cross(cell[0], cell[1]))
+                area = np.linalg.norm(np.cross(cell[0], cell[1]))
 
-                results[model_name].append((structs.get_potential_energy()-np.sum([model_mu[s] for s in symbols]))*1000/A)
-
-
+                results[model_name].append(
+                    (
+                        structs.get_potential_energy()
+                        - np.sum([model_mu[s] for s in symbols])
+                    )
+                    * 1000
+                    / area
+                )
 
                 # Copy individual structure files to app data directory
                 structs_dir = OUT_PATH / model_name
@@ -120,22 +127,24 @@ def slab_energies() -> dict[str, list]:
 
                 # Store reference energies (only once)
                 if not ref_stored:
-                    results["ref"].append((structs.info["DFT_energy"]-np.sum([ref_mu[s] for s in symbols]))*1000/A)
+                    results["ref"].append(
+                        (
+                            structs.info["DFT_energy"]
+                            - np.sum([ref_mu[s] for s in symbols])
+                        )
+                        * 1000
+                        / area
+                    )
 
         ref_stored = True
 
     return results
 
 
-
-
-
-
-
 @pytest.fixture
 def slab_positions() -> dict[str, list]:
     """
-    Get energies for all slabs systems.
+    Get positions for all slabs systems.
 
     Returns
     -------
@@ -157,13 +166,11 @@ def slab_positions() -> dict[str, list]:
 
         for xyz_file in xyz_files:
             name = xyz_file.name
-            if not (name.startswith('bulk') or name.startswith('gas_phase')):
+            if not (name.startswith("bulk") or name.startswith("gas_phase")):
                 structs = read(xyz_file, index=":")[0]
-                z_min = np.min(structs.positions[:,2])
-                moving = structs.positions[:,2]>z_min+0.1
+                z_min = np.min(structs.positions[:, 2])
+                moving = structs.positions[:, 2] > z_min + 0.1
                 results[model_name].append(structs.positions[moving])
-
-
 
                 # Store reference energies (only once)
                 if not ref_stored:
@@ -181,8 +188,8 @@ def ranking_error(slab_energies) -> dict[str, float]:
 
     Parameters
     ----------
-    relative_energies
-        Dictionary of reference and predicted relative energies.
+    slab_energies
+        Dictionary of reference and predicted surface energies.
 
     Returns
     -------
@@ -218,17 +225,15 @@ def ranking_error(slab_energies) -> dict[str, float]:
     return results
 
 
-
-
 @pytest.fixture
 def metal_surfaces_errors(slab_energies) -> dict[str, float]:
     """
-    Get mean absolute error for lattice energies.
+    Get mean absolute error for surface energies.
 
     Parameters
     ----------
-    lattice_energies
-        Dictionary of reference and predicted lattice energies.
+    slab_energies
+        Dictionary of reference and predicted surface energies.
 
     Returns
     -------
@@ -238,24 +243,21 @@ def metal_surfaces_errors(slab_energies) -> dict[str, float]:
     results = {}
     for model_name in MODELS:
         if slab_energies[model_name]:
-            results[model_name] = mae(
-                slab_energies["ref"], slab_energies[model_name]
-            )
+            results[model_name] = mae(slab_energies["ref"], slab_energies[model_name])
         else:
             results[model_name] = None
     return results
 
 
-
 @pytest.fixture
 def metal_position_errors(slab_positions) -> dict[str, float]:
     """
-    Get mean absolute error for lattice energies.
+    Get mean absolute error for positions.
 
     Parameters
     ----------
-    lattice_energies
-        Dictionary of reference and predicted lattice energies.
+    slab_positions
+        Dictionary of reference and predicted postitons.
 
     Returns
     -------
@@ -265,18 +267,17 @@ def metal_position_errors(slab_positions) -> dict[str, float]:
     results = {}
     for model_name in MODELS:
         if slab_positions[model_name]:
-            results[model_name] = (
-                np.mean(
-                    np.linalg.norm(
-                        np.concatenate(slab_positions["ref"])
-                        - np.concatenate(slab_positions[model_name]),
-                        axis=1,
-                    )
+            results[model_name] = np.mean(
+                np.linalg.norm(
+                    np.concatenate(slab_positions["ref"])
+                    - np.concatenate(slab_positions[model_name]),
+                    axis=1,
                 )
             )
         else:
             results[model_name] = None
     return results
+
 
 @pytest.fixture
 @build_table(
@@ -285,14 +286,22 @@ def metal_position_errors(slab_positions) -> dict[str, float]:
     thresholds=DEFAULT_THRESHOLDS,
     mlip_name_map=None,
 )
-def metrics(metal_surfaces_errors: dict[str, float], metal_position_errors: dict[str, float], ranking_error:  dict[str, float]) -> dict[str, dict]:
+def metrics(
+    metal_surfaces_errors: dict[str, float],
+    metal_position_errors: dict[str, float],
+    ranking_error: dict[str, float],
+) -> dict[str, dict]:
     """
-    Get all metal surface metrics.
+    Get all metal surface reconstructions metrics.
 
     Parameters
     ----------
     metal_surfaces_errors
-        Mean absolute errors for all systems.
+        Mean absolute errors for all surface energies.
+    metal_position_errors
+        Mean absolute errors for all positions.
+    ranking_error
+        Mean ranking error for all systems.
 
     Returns
     -------
@@ -305,9 +314,10 @@ def metrics(metal_surfaces_errors: dict[str, float], metal_position_errors: dict
         "Ranking Error": ranking_error,
     }
 
+
 def test_metal_surfaces(metrics: dict[str, dict]) -> None:
     """
-    Run metal surface test.
+    Run metal surface reconstructions test.
 
     Parameters
     ----------
