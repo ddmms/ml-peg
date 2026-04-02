fd03a8eedc3040e5781094d02c7748712b4d1e1f
Domantas Kuryla <116088428+kuryla@users.noreply.github.com>
2026-04-02 17:07:51 +0100

    Replace hardcoded density index with variable

--- ml_peg/analysis/molecular_dynamics/liquid_densities/analyse_liquid_densities.py
+++ ml_peg/analysis/molecular_dynamics/liquid_densities/analyse_liquid_densities.py
@@ -72,7 +72,7 @@ def compute_density(fname, density_col=13):
             items = line.strip().split()
             if len(items) != 15:
                 continue
-            density_series.append(float(items[13]))
+            density_series.append(float(items[density_col]))
     skip_frames = int(EQUILIB_TIME_PS / LOG_INTERVAL_PS)
     return np.mean(density_series[skip_frames:])
 
