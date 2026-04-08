cc73afbe877e55ff36348cf1ab85e97fcae13a67
Thomas Warford <thomasdwarford@hotmail.co.uk>
2026-04-08 21:57:25 +0100

    move code to split vacancy app

--- ml_peg/app/bulk_crystal/split_vacancy/app_split_vacancy.py
+++ ml_peg/app/bulk_crystal/split_vacancy/app_split_vacancy.py
@@ -2,16 +2,14 @@
 
 from __future__ import annotations
 
-from dash import Dash
-from dash.html import Div
+from dash import Dash, Input, Output, callback
+from dash.html import Div, Iframe
 
 from ml_peg.app import APP_ROOT
 from ml_peg.app.base_app import BaseApp
-from ml_peg.app.utils.build_callbacks import (
-    plot_from_table_column,
-    struct_pair_from_violin,
-)
+from ml_peg.app.utils.build_callbacks import plot_from_table_column
 from ml_peg.app.utils.load import read_plot
+from ml_peg.app.utils.weas import generate_weas_html
 from ml_peg.models.get_models import get_model_names
 from ml_peg.models.models import current_models
 
@@ -21,6 +19,88 @@ BENCHMARK_NAME = "Split vacancy"
 # TODO: change DOCS_URL
 DOCS_URL = "https://ddmms.github.io/ml-peg/user_guide/benchmarks/bulk_crystal.html#lattice-constants"
 DATA_PATH = APP_ROOT / "data" / "bulk_crystal" / "split_vacancy"
+
+# for dash, assets/ is equivalent to APP_ROOT/data/
+STRUCTS_URL = "assets/bulk_crystal/split_vacancy"
+
+
+def struct_pair_from_violin(
+    violin_id: str,
+    struct_id: str,
+    functional: str,
+) -> None:
+    """
+    Register callback to show ref and MLIP structures when a violin point is clicked.
+
+    Parameters
+    ----------
+    violin_id
+        ID for Dash violin plot being clicked.
+    struct_id
+        ID for Dash placeholder Div where structures will be visualised.
+    functional
+        DFT functional (``"pbe"`` or ``"pbesol"``), used to determine structure paths.
+    """
+
+    @callback(
+        Output(struct_id, "children", allow_duplicate=True),
+        Input(violin_id, "clickData"),
+        prevent_initial_call="initial_duplicate",
+    )
+    def show_struct(click_data):
+        """
+        Register callback to show structures when point clicked. See build_callbacks.py.
+
+        Parameters
+        ----------
+        click_data
+            Clicked data point in scatter plot.
+
+        Returns
+        -------
+        Div
+            Visualised structure on plot click.
+        """
+        if not click_data:
+            return Div("Click on a point to view structures.")
+
+        point = click_data["points"][0]
+        model_name = point["x"]
+        mp_id = point["customdata"][0]
+        formula = point["customdata"][1]
+        cation = point["customdata"][2]
+        vac_type = point["customdata"][3]
+        frame_id = int(point["customdata"][4])
+
+        xyz_name = "normal_vacancy.xyz" if vac_type == "NV" else "split_vacancy.xyz"
+        material_dir = f"{formula}-{mp_id}"
+
+        prefix = f"{STRUCTS_URL}/{functional}"
+        ref_struct = f"{prefix}/ref/{material_dir}/{cation}/{xyz_name}"
+        mlip_struct = f"{prefix}/{model_name}/{material_dir}/{cation}/{xyz_name}"
+
+        ref_html = generate_weas_html(ref_struct, mode="traj", index=frame_id)
+        mlip_html = generate_weas_html(mlip_struct, mode="traj", index=frame_id)
+
+        iframe_style = {
+            "height": "550px",
+            "width": "100%",
+            "border": "1px solid #ddd",
+            "borderRadius": "5px",
+        }
+        return Div(
+            [
+                Div(
+                    [Iframe(srcDoc=ref_html, style=iframe_style)],
+                    style={"width": "50%", "paddingRight": "4px"},
+                ),
+                Div(
+                    [Iframe(srcDoc=mlip_html, style=iframe_style)],
+                    style={"width": "50%", "paddingLeft": "4px"},
+                ),
+            ],
+            style={"display": "flex"},
+        )
 
 
 class SplitVacancyApp(BaseApp):
