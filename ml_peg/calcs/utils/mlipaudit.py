33e99de171a8854fe5ed5fc3f584fada870e5b32
Leon Wehrhan <mail@leonwehrhan.de>
2026-06-23 23:19:54 +0200

    feat: update MAE per molecule calculation

--- ml_peg/calcs/utils/mlipaudit.py
+++ ml_peg/calcs/utils/mlipaudit.py
@@ -12,8 +12,7 @@ class MlPegConformerSelectionBenchmark(ConformerSelectionBenchmark):
     ConformerSelectionBenchmark wired up for ml-peg's ASE calculators.
 
     ``skip_if_elements_missing`` is disabled because ASE ``Calculator`` objects
-    do not expose ``allowed_atomic_numbers``. ml-peg manages model/element
-    compatibility separately via its model registry.
+    do not expose ``allowed_atomic_numbers``.
     """
 
     skip_if_elements_missing = False
