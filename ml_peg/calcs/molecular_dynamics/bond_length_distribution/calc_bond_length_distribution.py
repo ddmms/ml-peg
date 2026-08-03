37738a3cd279128d95195c03cdd9d5ddeab12ea0
lwalew <l.walewski@instadeep.com>
2026-08-03 10:22:33 +0200

    feat: skip if mlipaudit not installed

--- ml_peg/calcs/molecular_dynamics/bond_length_distribution/calc_bond_length_distribution.py
+++ ml_peg/calcs/molecular_dynamics/bond_length_distribution/calc_bond_length_distribution.py
@@ -12,11 +12,13 @@ from pathlib import Path
 from typing import Any
 from warnings import warn
 
+import pytest
+
+pytest.importorskip("mlipaudit", reason="Please install `mlipaudit` extra")
 from mlipaudit.benchmarks.bond_length_distribution.bond_length_distribution import (
     BondLengthDistributionModelOutput,
 )
 from mlipaudit.io import write_model_output_to_disk
-import pytest
 
 from ml_peg.calcs.utils.mlipaudit import MlPegBondLengthDistributionBenchmark
 from ml_peg.calcs.utils.utils import download_s3_data
