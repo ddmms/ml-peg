304b63af0c03f0e46b34fd5ca63210452344ba06
lwalew <l.walewski@instadeep.com>
2026-09-15 13:53:02 +0200

    feat: use low precision for MD

--- ml_peg/calcs/biomolecules/protein_folding_stability/calc_protein_folding_stability.py
+++ ml_peg/calcs/biomolecules/protein_folding_stability/calc_protein_folding_stability.py
@@ -55,7 +55,7 @@ def test_protein_folding_stability(mlip: tuple[str, Any]) -> None:
         Name of model and model object to get calculator.
     """
     model_name, model = mlip
-    calc = model.get_calculator()
+    calc = model.get_calculator(precision="low")
     calc = model.add_d3_calculator(calc)
 
     data_input_dir = download_s3_data(
